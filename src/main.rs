1c010376aff198125607aeac231de1e4ad407cb8
Joonkyo Kim <rootwarp@gmail.com>
2022-12-01 23:00:15 +0900

    Query ATOM account

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use assets::cosmos::{CosmoshubAccount, Account};
+
+#[tokio::main]
+async fn main() {
+    let account = env::var("ACCOUNT").unwrap_or("".to_string());
+
+    let cosmos = CosmoshubAccount{address: String::from(account)};
+    let ret = cosmos.balance().await;
+
+    match ret {
+        Ok(balance) => {
+            println!("{:?}", balance);
+        }
+
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
 }
