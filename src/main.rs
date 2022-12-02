41ee3d31a12bd588829d1f365ee702a6477b58ea
Joonkyo Kim <rootwarp@gmail.com>
2022-12-02 18:14:57 +0900

    Cosmos staking and reward balance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use futures;
 
 use assets::cosmos::{CosmoshubAccount, Account};
 
@@ -7,11 +8,31 @@ async fn main() {
     let account = env::var("ACCOUNT").unwrap_or("".to_string());
 
     let cosmos = CosmoshubAccount{address: String::from(account)};
-    let ret = cosmos.balance().await;
+    let bal = cosmos.balance();
+    let staking = cosmos.staking();
+    let rewards = cosmos.rewards();
+
+    let ret = futures::try_join!(bal, staking, rewards);
 
     match ret {
-        Ok(balance) => {
-            println!("{:?}", balance);
+        Ok(values) => {
+            let balance: f64 = values.0.iter()
+                .map(|b| { b.amount.parse::<f64>().unwrap() })
+                .sum();
+
+            let staking: f64 = values.1.iter()
+                .map(|v| { v.balance.amount.parse::<f64>().unwrap() })
+                .sum();
+
+            let reward: f64 = values.2.iter()
+                .map(|v| { v.amount.parse::<f64>().unwrap() })
+                .sum();
+            
+            let sum: f64 = [balance, staking, reward].iter().sum();
+
+            println!("{:?}", values);
+            println!("{:?} {:?} {:?}", balance, staking, reward);
+            println!("Total {:?}", sum);
         }
 
         Err(err) => {
