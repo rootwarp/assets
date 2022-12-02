4761b454729406f6c97500dcf660d2c7fd051823
Joonkyo Kim <rootwarp@gmail.com>
2022-12-02 18:14:57 +0900

    Cosmos staking and reward balance

--- src/cosmos.rs
+++ src/cosmos.rs
@@ -7,18 +7,50 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BalanceResponse {
-    balances: Vec<Balance>,
+    pub balances: Vec<Balance>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Balance {
-    denom: String,
-    amount: String,
+    pub denom: String,
+    pub amount: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DelegateResponse {
+    pub delegation_responses: Vec<DelegateData>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DelegateData {
+    pub delegation: Delegation,
+    pub balance: Balance,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Delegation {
+    pub delegator_address: String,
+    pub validator_address: String,
+    pub shares: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RewardsResponse {
+    pub rewards: Vec<RewardsData>,
+    pub total: Vec<Balance>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RewardsData {
+    pub validator_address: String,
+    pub reward: Vec<Balance>,
 }
 
 #[async_trait]
 pub trait Account {
     async fn balance(&self) -> Result<Vec<Balance>, Box<dyn Error>>;
+    async fn staking(&self) -> Result<Vec<DelegateData>, Box<dyn Error>>;
+    async fn rewards(&self) -> Result<Vec<Balance>, Box<dyn Error>>;
 }
 
 pub struct CosmoshubAccount {
@@ -35,9 +67,51 @@ impl Account for CosmoshubAccount {
         match response {
             Ok(resp) => {
                 let body = resp.text().await?;
+                println!("balance {}", body);
+
                 let ret: serde_json::Result<BalanceResponse> = serde_json::from_str(&*body);
                 match ret {
                     Ok(balance) => Ok(balance.balances),
+                    Err(err) => Err(Box::new(err)),
+                }
+            }
+            Err(err) => Err(Box::new(err))
+        }
+    }
+
+    async fn staking(&self) -> Result<Vec<DelegateData>, Box<dyn Error>> {
+        let base = "https://cosmos-mainnet-rpc-korea.allthatnode.com:1317";
+
+        let url = format!("{}/cosmos/staking/v1beta1/delegations/{}", base, self.address);
+        let response = reqwest::get(url).await;
+        match response {
+            Ok(resp) => {
+                let body = resp.text().await?;
+                println!("staking {}", body);
+
+                let ret: serde_json::Result<DelegateResponse> = serde_json::from_str(&*body);
+                match ret {
+                    Ok(data) => Ok(data.delegation_responses),
+                    Err(err) => Err(Box::new(err)),
+                }
+            }
+            Err(err) => Err(Box::new(err))
+        }
+    }
+
+    async fn rewards(&self) -> Result<Vec<Balance>, Box<dyn Error>> {
+        let base = "https://cosmos-mainnet-rpc-korea.allthatnode.com:1317";
+
+        let url = format!("{}/cosmos/distribution/v1beta1/delegators/{}/rewards", base, self.address);
+        let response = reqwest::get(url).await;
+        match response {
+            Ok(resp) => {
+                let body = resp.text().await?;
+                println!("rewards {}", body);
+
+                let ret: serde_json::Result<RewardsResponse> = serde_json::from_str(&*body);
+                match ret {
+                    Ok(rewards) => Ok(rewards.total),
                     Err(err) => Err(Box::new(err)),
                 }
             }
